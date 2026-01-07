41e2ecd786d6a08b7dadacc46d93fe9deee5091f
AleksandrovIgor1 <alexigore@yandex.ru>
2026-01-07 13:08:18 +0500

    Pagination v1

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,22 +3,27 @@ import styles from './styles.module.css'
 import NewsBanner from '../../components/NewsBanner/NewsBanner'
 import { getNews } from '../../api/apiNews'
 import NewsList from '../../components/NewsList/NewsList'
+import Pagination from '../../components/Pagination/Pagination'
 function Main() {
     const [news, setNews] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPage = 10
+    const pageSize = 10
     useEffect(() => {
         const fetchNews = async () => {
             try {
-                const response = await getNews()
+                const response = await getNews(currentPage, pageSize)
                 setNews(response.news)
             } catch (error) {
                 console.log(error)
             }
         }
-        fetchNews()
-    }, [])
+        fetchNews(currentPage)
+    }, [currentPage])
     return (
     <main className={styles.main}>
         {news.length > 0 ? <NewsBanner item={news[0]}/> : null}
+        <Pagination/>
         <NewsList news={news} />
     </main>
     )
