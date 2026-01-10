664d3de070e4bf0408cbabd6e4934f069571bf3b
AleksandrovIgor1 <alexigore@yandex.ru>
2026-01-10 13:02:11 +0500

    add context and change theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,19 @@
+import { themeIcons } from "../../assets";
 import { formatDate } from "../../helpers/hooks/formatDate";
 import styles from "./styles.module.css";
+import { useTheme } from "../../context/ThemeContext";
+
+const { isDark, toggleTheme } = useTheme()
+
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <h1 className={styles.title}>Good morning</h1>
-      <p className={styles.date}>{formatDate(new Date())}</p>
+    <header className={`${styles.header} ${isDark ? styles.dark : styles.light}`}>
+      <div className={styles.info}>
+        <h1 className={styles.title}>Good morning</h1>
+        <p className={styles.date}>{formatDate(new Date())}</p>
+      </div>
+      <img src={isDark ? themeIcons.dark : themeIcons.light} width={30} alt="theme" onClick={toggleTheme} />
+
     </header>
   );
 };
