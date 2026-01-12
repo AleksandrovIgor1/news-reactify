aa2344d29e932c319e6b7dc9be9651c2fd8b1869
AleksandrovIgor1 <alexigore@yandex.ru>
2026-01-12 18:19:06 +0500

    add news page and react router

--- src/pages/main/ui/LatestNews/LatestNews.tsx
+++ src/pages/main/ui/LatestNews/LatestNews.tsx
@@ -1,11 +1,29 @@
 import { useGetLatestNewsQuery } from '@/entities/news/api/newsApi';
 import styles from './styles.module.css';
 import { NewsList } from '@/widgets/news';
+import type { INews } from '@/entities/news';
+import { useAppDispatch } from '@/app/appStore';
+import { setCurrentNews } from '@/entities/news/model/newsSlice';
+import { useNavigate } from 'react-router-dom';
 const LatestNews = () => {
     const { data, isLoading } = useGetLatestNewsQuery(null)
+
+    const dispath = useAppDispatch()
+    const navigate = useNavigate();
+
+    const navigateTo = (news: INews) => {
+        dispath(setCurrentNews(news))
+        navigate(`/news/${news.id}`)
+    }
     return (
         <section className={styles.section}>
-            <NewsList isLoading={isLoading} type='banner' direction='row' news={data && data.news} />
+            <NewsList
+                isLoading={isLoading}
+                type='banner'
+                direction='row'
+                news={data && data.news}
+                viewNewsSlot={(news: INews) => <p onClick={() => navigateTo(news)}
+                >view more...</p>} />
         </section>
     )
 }
